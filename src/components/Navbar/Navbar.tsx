f9eefda9406507eb5b69fd64775087c01069a35b
GrazynaDachtera <grazynadachtera@gmail.com>
2025-10-20 21:34:47 +0100

    removed horizontal navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,27 +1,21 @@
 "use client";
 
-import React from "react";
 import Image from "next/image";
-import { Flex } from "@chakra-ui/react";
+import Link from "next/link";
 import "./Navbar.scss";
 
-const NavBar: React.FC = () => {
+export default function NavBar() {
   return (
-    <Flex className="wrapper-navbar">
-      <nav className="navbar">
-        <div className="logo-navbar">
-          <Image
-            src="/Navbar/gorilla.svg"
-            width={58}
-            height={60}
-            className="Kongwellogo"
-            alt="Kongwellogo"
-            priority
-          />
-        </div>
-      </nav>
-    </Flex>
+    <div className="brand-mark" role="banner" aria-label="Site logo">
+      <Link href="/" aria-label="Go to homepage">
+        <Image
+          src="/Navbar/gorilla.svg"
+          width={58}
+          height={60}
+          alt="Kongwello logo"
+          priority
+        />
+      </Link>
+    </div>
   );
-};
-
-export default NavBar;
+}
